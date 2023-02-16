2f8a5fe38979117eb9a348f1d04103e07d5b27ab
secc9 <78687901+secc9@users.noreply.github.com>
2023-02-16 17:00:42 +0000

    added a sketch.js as proof of concept and linked to each user

---
+++ assets/paula/sketch.js
@@ -0,0 +1,19 @@
+let blue = 150;
+let canvas;
+function setup(){
+  canvas = createCanvas(1000, 1000)
+canvas.position(0, 0);
+canvas.style('z-index', '-1');
+}
+
+function draw(){
+  background(200);
+  noStroke()
+  fill(0, 24, blue);
+  ellipse(width/2, height/2, 200, 200);
+
+  if(blue < 0){
+    blue = 150;
+  }
+  blue = blue - 1;
+}
