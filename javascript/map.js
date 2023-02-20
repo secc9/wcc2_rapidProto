c4754ecaadb69a00fe3cd65726a75a47ad828a2f
ievavaiti123 <110530476+ievavaiti123@users.noreply.github.com>
2023-02-20 12:18:50 +0000

    Added Map (pls read)
    I added a Leaflet map, however I would like to possibly use another library, such as tilemap.js to give us more freedom and functionality. Unfortunately mappa.js is now obsolete.

---
+++ javascript/map.js
@@ -0,0 +1,22 @@
+// let myMap;
+// // Create a variable to hold our canvas
+// let canvas;
+// // Create a new Mappa instance using Leaflet.
+// const mappa = new Mappa('Leaflet');
+
+// p5.js setup
+function setup(){
+//   // Create a canvas 640x640
+//   canvas = createCanvas(640,640); 
+//   // Add a grey background
+//   background(100);
+//    // Create a tile map with lat 0, lng 0, zoom 4
+//    myMap = mappa.tileMap(0,0,4); 
+//    // Overlay the canvas over the tile map
+//    myMap.overlay(canvas);
+}
+
+// p5.js draw
+function draw(){
+
+}
