4678e63d1bd5d592bc3bbf8c4b6bcbde70448c47
ievavaiti123 <110530476+ievavaiti123@users.noreply.github.com>
2023-02-28 14:04:27 +0000

    moved everything to map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,22 +1,75 @@
- let myMap;
- // Create a variable to hold our canvas
- let canvas;
- // Create a new Mappa instance using Leaflet.
- const mappa = new Mappa('Leaflet');
+// src="./javascript/map.js"
+        const map = L.map('map').setView([51.505, -0.09], 3);
 
-// p5.js setup
-function setup(){
-   // Create a canvas 640x640
-   canvas = createCanvas(640,640); 
-   // Add a grey background
-  background(100);
-   // Create a tile map with lat 0, lng 0, zoom 4
-       myMap = mappa.tileMap(0,0,4); 
-    // Overlay the canvas over the tile map
-        myMap.overlay(canvas);
-}
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-// p5.js draw
-function draw(){
+// <!-- paula -->
+// icon image
 
-}
+
+/////////////////////IEVA
+const ieva = L.icon({
+    iconUrl: './assets/images/IV selfie.jpg',
+    iconSize: [32,32],
+    iconAnchor:[25,16]
+})
+
+
+
+
+// marker of tate
+var marker = L.marker([51.507685, -0.099073], {icon: ieva}).addTo(map);
+
+
+
+var popup = L.popup()
+    .setLatLng([51.507685, -0.099073])
+    .setContent("Tate Modern")
+    .openOn(map);
+
+
+    
+
+
+//////////////////PAULA
+
+
+
+////////////////////KRUTI
+
+
+
+
+
+
+
+
+///////////////ASHISH
+
+
+
+
+
+
+
+
+
+
+//////////////ASH
+
+const ash = L.icon({
+  iconUrl: './assets/images/ash_image.jpeg',
+  iconSize: [32,32],
+  iconAnchor:[25,16]
+})
+
+// marker of leeds
+var marker = L.marker([53.828217, -1.566547], {icon: ash}).addTo(map);
+
+var popup = L.popup()
+  .setLatLng([51.507685, -0.099073])
+  .setContent("Tate Modern")
+  .openOn(map);
