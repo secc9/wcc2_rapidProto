4777ee4ca7aa04fac6b49fbc9085ab76a0337389
96kruti <120492921+96kruti@users.noreply.github.com>
2023-02-28 14:37:29 +0000

    Added the location in india

--- javascript/map.js
+++ javascript/map.js
@@ -39,7 +39,85 @@ var popup = L.popup()
 
 
 ////////////////////KRUTI
+/////Jaipur
+const kruti = L.icon({
+  iconUrl: './assets/images/jaipur.jpg',//change picture
+  iconSize: [32,32],
+  iconAnchor:[25,16]
+})
 
+
+
+
+// marker of Jaipur
+var marker = L.marker([26.922070, 75.778885], {icon: kruti}).addTo(map);
+
+
+
+var popup = L.popup()
+  .setLatLng([26.922070, 75.778885])
+  .setContent("Jaipur")
+  .openOn(map);
+
+/////Auroville
+const kruti2 = L.icon({
+  iconUrl: './assets/images/auroville.jpg',//change picture
+  iconSize: [32,32],
+  iconAnchor:[25,16]
+})
+
+
+
+
+// marker of Auroville
+var marker = L.marker([11.9352, 79.8238], {icon: kruti2}).addTo(map);
+
+
+
+var popup = L.popup()
+  .setLatLng([11.9352, 79.8238])
+  .setContent("Auroville")
+  .openOn(map);
+
+/////Mumbai
+const kruti3 = L.icon({
+  iconUrl: './assets/images/Mumbai.jpg',//change picture
+  iconSize: [32,32],
+  iconAnchor:[25,16]
+})
+
+
+
+
+// marker of Mumbai
+var marker = L.marker([19.076090, 72.877426], {icon: kruti3}).addTo(map);
+
+
+
+var popup = L.popup()
+  .setLatLng([19.076090, 72.877426])
+  .setContent("Mumbai")
+  .openOn(map);
+
+/////Hampi
+const kruti4 = L.icon({
+  iconUrl: './assets/images/hampi.jpg',//change picture
+  iconSize: [32,32],
+  iconAnchor:[25,16]
+})
+
+
+
+
+// marker of Hampi
+var marker = L.marker([15.334022, 76.468536], {icon: kruti4}).addTo(map);
+
+
+
+var popup = L.popup()
+  .setLatLng([15.334022, 76.468536])
+  .setContent("Hampi")
+  .openOn(map);
 
 
 
